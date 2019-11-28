794b466efb8ab95e37b75493f7bad1f38a2ac4b7
huanfuan <huanfuan@163.com>
2019-11-28 13:56:24 +0800

    hello-demo

---
+++ main.go
@@ -0,0 +1,18 @@
+/*
+@Time : 2019/11/28 0028 上午 11:41
+@Author : huanfuan
+@File : main
+@Software: GoLand
+*/
+
+package main
+
+
+import "github.com/gin-gonic/gin"
+
+func main()  {
+	router := gin.Default()
+	router.Run()
+}
+
+
