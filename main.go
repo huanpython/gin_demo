8a701e35b9f3b6071467325494ee6272fc313cb6
huanfuan <huanfuan@163.com>
2019-12-03 16:35:10 +0800

    gin docker示例

--- main.go
+++ main.go
@@ -9,11 +9,13 @@ package main
 
 import (
 	"gin-demo/initRouter"
+	"github.com/gin-gonic/gin"
 )
 
 //https://youngxhui.top/2019/07/gin%E4%B9%9D%E7%94%9F%E6%88%90restful%E6%8E%A5%E5%8F%A3/
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	router := initRouter.SetupRouter()
 	_ = router.Run()
 
